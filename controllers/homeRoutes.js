27322f782cc0379b71eddbbeaeb5950f7e7c7c8e
allenmurfee <allenmurfee123@gmail.com>
2022-11-19 11:04:03 -0500

    working on initial setup

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,6 @@
+router.get("/", async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-require('dotenv').config();
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+require("dotenv").config();
 
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: process.env.SECRET,
@@ -24,17 +26,15 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
